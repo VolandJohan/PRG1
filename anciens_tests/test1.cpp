a32a5d6fc51d074e12a16626eefc0cac4767b2c1
Johan Voland <jvoland99@gmail.com>
2022-01-29 15:09:56 +0100

    Création dossier des tests

---
+++ anciens_tests/test1.cpp
@@ -0,0 +1,9 @@
+/************************************
+ * Execice 1 - identificateurs valides
+*************************************/
+
+/*#include <iostream>
+
+using namespace std;
+
+int main() {}*/
